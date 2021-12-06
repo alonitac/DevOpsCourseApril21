ec03464e42e8199bdbb3162a3dc6e3cf18819e24
Alex Gorbachov <agorbachov@gmail.com>
2021-12-06 17:23:46 +0200

    Editted access-manager.py, simple upload s3.py, created a credentials.py file.danielitzakian/aws_ex2

---
+++ credentials.py
@@ -0,0 +1,13 @@
+#CREDENTIALS
+
+#TODO Define Bucket Name to upload Videos
+bucket = 'yt-buck'
+
+# TODO Define An Admin User That Will Not Be Deleted
+admin = 'YoutubeDownloader'
+
+#TODO Define User Maximun Subscriber age
+max_user_age_seconds = (2.0)
+
+#TODO Define Policy To set Users Permissions
+permission = 'arn:aws:iam::736863849176:policy/S3VideoReader'
