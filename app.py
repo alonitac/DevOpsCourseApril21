48ac51d8d86cec53777cce39ad17cbf146ea7cf3
Alon Itach <alon.itach@mail.huji.ac.il>
2022-01-27 19:33:18 +0200

    liveness probe

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+req_count = 1
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/healthz")
+def health_check():
+    global req_count
+
+    req_count += 1
+    if req_count > 40:
+        return "", 500
+    else:
+        return "", 200
 
 
 @app.route("/<name>")
