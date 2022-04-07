263fb18c050ae2bc98ead2ff1ddc835be9092001
alonit <alon.itach@mail.huji.ac.il>
2022-04-07 18:35:06 +0300

    build & publish package, integrate in jenkinsfile

--- package_demo/setup.py
+++ package_demo/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='fantastic_ascii2',
-    version='1.0.0',
+    version='1.0.1',
     description='Fantastic ASCII II',
     license='MIT',
     packages=find_packages(),
